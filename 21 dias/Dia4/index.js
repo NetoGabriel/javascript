f93750fa3eb817a15caebc6745f78d97c448b1b4
Gabriel Neto <167437469+NetoGabriel@users.noreply.github.com>
2024-10-15 22:47:50 -0300

    atividade do dia 15/10

---
+++ 21 dias/Dia4/index.js
@@ -0,0 +1,17 @@
+//exemplo
+
+//let tabuada= Number(prompt('escolha a tabuada que deseja'))
+
+//for(let contador = 0; contador <= 20; contador++){
+  //  console.log(tabuada + "x" + contador + "=" + tabuada*contador)}
+
+
+  let valor= parseInt(prompt("Digite um valor inteiro positivo"))
+
+  for(let cont = 0; cont <= valor; cont++){
+    console.log(cont)
+  }
+
+  for(let cont = 0; cont <= 50; cont += 5){
+    console.log(cont)
+  }
---
+++ 21 dias/Dia4/index2.js
@@ -0,0 +1,19 @@
+//exemplo
+
+//let tabuada= Number(prompt('escolha a tabuada que deseja'))
+
+//for(let contador = 0; contador <= 20; contador++){
+  //  console.log(tabuada + "x" + contador + "=" + tabuada*contador)}
+  for(let i = 50; i >= 0; i -= 5)
+    console.log(i)
+
+  let numero= parseInt(prompt("Digite um numero"))
+    for(let i = numero; i <= numero + 2; i++){
+        console.log("Tabuada" + i)
+    }
+    for(let k = 0; k <= 10; k++){
+        console.log(
+           i + "x" + k + "=" + (i * k))
+    }
+
+
