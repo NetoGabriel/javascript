17a07e5a6417902e4e0781ae081a82b628b90b2c
Gabriel Neto <167437469+NetoGabriel@users.noreply.github.com>
2024-11-16 22:09:55 -0300

    atividade de js

---
+++ guacurso/ex02/index.js
@@ -0,0 +1,7 @@
+var pais = ("eua")
+console.log(`voce mora no ${pais}`)
+if (pais != "brasil"){
+    console.log("Voce e estrageira")}
+else{"Voce e estrangeiro"
+    console.log("Voce e brasileiro")
+}
