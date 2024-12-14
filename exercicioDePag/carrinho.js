c26439ef715fbbb132a22d6e3c781c7d85b61c05
Gabriel Neto <josegabriel9211@gmail.com>
2024-12-14 16:38:09 -0300

    atualização(refazendo)

--- exercicioDePag/carrinho.js
+++ exercicioDePag/carrinho.js
@@ -1,3 +1,21 @@
-function adccarro(){
-    
-}
+const openbtn = document.querySelectorAll('.open-modal');
+
+openbtn.forEach(btn =>{
+    btn.addEventListener('click', () =>{
+        const modalid = btn.getAttribute('data-modal');
+        const modal = document.getElementById(modalid);
+        modal.showModal()
+    });
+});
+
+const closebtn = document.querySelectorAll('.close-modal');
+
+closebtn.forEach(btn =>{
+    btn.addEventListener('click', () =>{
+        const modalid = btn.getAttribute('data-modal');
+        const modal = document.getElementById(modalid);
+        modal.close();
+    })
+})
+
+
