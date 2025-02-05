1559344b46d12f96bd5dd0e2ba586e73da450b14
Gabriel Neto <josegabriel9211@gmail.com>
2025-02-05 16:43:41 -0300

    atualizacao

---
+++ exercicioDePag/js/home.js
@@ -0,0 +1,19 @@
+class Roupas{
+    Produto 
+    Roupas
+    Img
+    Quantidade
+    Id
+    constructor(produto, roupas, img, id){
+        this.Produto = produto
+        this.Roupas = roupas
+        this.Img = img
+        this.Quantidade = 0
+        this.Id = id
+    }
+}
+
+const roupas1 = new Roupas('Camiseta', 'Preta', 'img/camisetaPreta.jpg', 1)
+const roupas2 = new Roupas
+const roupas3 = new Roupas
+const roupas4 = new Roupas
