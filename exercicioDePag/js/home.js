b90473a8aa83f0f874b46d63c48de4fdc8b6bd29
Gabriel Neto <josegabriel9211@gmail.com>
2025-02-07 22:40:24 -0300

    Atualizacao do siteDev

--- exercicioDePag/js/home.js
+++ exercicioDePag/js/home.js
@@ -1,76 +1,105 @@
-class Roupas{
-    Produto 
-    Roupas
-    Img
-    Quantidade
-    Id
-    constructor(produto, roupas, img, id){
-        this.Produto = produto
-        this.Roupas = roupas
-        this.Img = img
-        this.Quantidade = 0
-        this.Id = id
+class Roupas {
+    Produto;
+    Roupas;
+    Img;
+    Quantidade;
+    Id;
+    constructor(produto, roupas, img, id) {
+        this.Produto = produto;
+        this.Roupas = roupas;
+        this.Img = img;
+        this.Quantidade = 0;
+        this.Id = id;
     }
 }
 
-const roupas1 = new Roupas('Camiseta Preta', 100 , 'img/camisetaPreta.jpg','img/camisetaPreta.jpg', 'img/camisetaPreta.jpg', 'img/camisetaPreta.jpg' , 1)
-const roupas2 = new Roupas('Moleton Branco', 200 , 'img/moletonBranco.jpg','img/moletonBranco.jpg', 'img/moletonBranco.jpg', 'img/moletonBranco.jpg' , 2)
-const roupas3 = new Roupas('Calça Jeans', 150 , 'img/calçaJeans.jpg','img/calçaJeans.jpg', 'img/calçaJeans.jpg', 'img/calçaJeans.jpg' , 3)
-const roupas4 = new Roupas('Moletom Preto', 200 , 'img/moletomPreto.jpg','img/moletomPreto.jpg', 'img/moletomPreto.jpg', 'img/moletomPreto.jpg' , 4)
+const roupas1 = new Roupas('Camiseta Preta', 100, ['img/camisetaPreta.jpg'], 1);
+const roupas2 = new Roupas('Moleton Branco', 200, ['img/moletonBranco.jpg'], 2);
+const roupas3 = new Roupas('Calça Jeans', 150, ['img/calçaJeans.jpg'], 3);
+const roupas4 = new Roupas('Moletom Preto', 200, ['img/moletomPreto.jpg'], 4);
 
-const btn1 = document.getElementById('btn1')
-
-btn1.addEventListener('click', () => {
-    window.location.replace('produtos.html')
-
-    window.localStorage.setItem('Nome',roupas1.Produto)
-    window.localStorage.setItem('Preço',roupas1.Preco)
-    window.localStorage.setItem("id",roupas1.Id)
-        for (let i = 0; i < 4; i++) {
-            window.localStorage.setItem(('img' + (1 + 1)), roupas1.Img[i])
-            
+const btn1 = document.getElementById('btn1');
+if (btn1) {
+    btn1.addEventListener('click', () => {
+        window.location.replace('produtos.html');
+        window.localStorage.setItem('Nome', roupas1.Produto);
+        window.localStorage.setItem('Preço', roupas1.Roupas);
+        window.localStorage.setItem('id', roupas1.Id);
+        for (let i = 0; i < roupas1.Img.length; i++) {
+            window.localStorage.setItem('img' + (i + 1), roupas1.Img[i]);
         }
-})
+    });
+}
 
-const btn2 = document.getElementById('btn2')
-
-btn2.addEventListener('click', () => {
-    window.location.replace('produtos.html')
-
-    window.localStorage.setItem('Nome',roupas2.Produto)
-    window.localStorage.setItem('Preço',roupas2.Preco)
-    window.localStorage.setItem("id",roupas2.Id)
-        for (let i = 0; i < 4; i++) {
-            window.localStorage.setItem(('img' + (1 + 1)), roupas2.Img[i])
-            
+const btn2 = document.getElementById('btn2');
+if (btn2) {
+    btn2.addEventListener('click', () => {
+        window.location.replace('produtos.html');
+        window.localStorage.setItem('Nome', roupas2.Produto);
+        window.localStorage.setItem('Preço', roupas2.Roupas);
+        window.localStorage.setItem('id', roupas2.Id);
+        for (let i = 0; i < roupas2.Img.length; i++) {
+            window.localStorage.setItem('img' + (i + 1), roupas2.Img[i]);
         }
-})
+    });
+}
 
-const btn3 = document.getElementById('btn3')
-
-btn3.addEventListener('click', () => {
-    window.location.replace('produtos.html')
-
-    window.localStorage.setItem('Nome',roupas3.Produto)
-    window.localStorage.setItem('Preço',roupas3.Preco)
-    window.localStorage.setItem("id",roupas3.Id)
-        for (let i = 0; i < 4; i++) {
-            window.localStorage.setItem(('img' + (1 + 1)), roupas3.Img[i])
-            
+const btn3 = document.getElementById('btn3');
+if (btn3) {
+    btn3.addEventListener('click', () => {
+        window.location.replace('produtos.html');
+        window.localStorage.setItem('Nome', roupas3.Produto);
+        window.localStorage.setItem('Preço', roupas3.Roupas);
+        window.localStorage.setItem('id', roupas3.Id);
+        for (let i = 0; i < roupas3.Img.length; i++) {
+            window.localStorage.setItem('img' + (i + 1), roupas3.Img[i]);
         }
-})
+    });
+}
 
-const btn4 = document.getElementById('btn4')
-
-btn4.addEventListener('click', () => {
-    window.location.replace('produtos.html')
-
-    window.localStorage.setItem('Nome',roupas4.Produto)
-    window.localStorage.setItem('Preço',roupas4.Preco)
-    window.localStorage.setItem("id",roupas4.Id)
-        for (let i = 0; i < 4; i++) {
-            window.localStorage.setItem(('img' + (1 + 1)), roupas4.Img[i])
-            
+const btn4 = document.getElementById('btn4');
+if (btn4) {
+    btn4.addEventListener('click', () => {
+        window.location.replace('produtos.html');
+        window.localStorage.setItem('Nome', roupas4.Produto);
+        window.localStorage.setItem('Preço', roupas4.Roupas);
+        window.localStorage.setItem('id', roupas4.Id);
+        for (let i = 0; i < roupas4.Img.length; i++) {
+            window.localStorage.setItem('img' + (i + 1), roupas4.Img[i]);
         }
-})
+    });
+}
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('contato');
 
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Impede o envio padrão do formulário
+
+        const nome = document.getElementById('inome').value;
+        const email = document.getElementById('iemail').value;
+        const telefone = document.getElementById('itelefone').value;
+
+        const data = {
+            nome: nome,
+            email: email,
+            telefone: telefone
+        };
+
+        fetch('https://apigenerator.dronahq.com/api/72BE1GmG/SiteDev', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            // Aqui você pode adicionar código para lidar com a resposta da API
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            // Aqui você pode adicionar código para lidar com erros
+        });
+    });
+});
--- exercicioDePag/js/produto.js
+++ exercicioDePag/js/produto.js
@@ -1,64 +1,61 @@
 const roupa = document.querySelector('#roupa');
 const valor = document.querySelector('#valor');
-const imagem = document.querySelector('#img');
-const imgGrande = document.querySelector('img4');
+const imgGrande = document.querySelector('#img4');
 
 window.addEventListener('load', e => {
-    roupas.innerHTML = localStorage.getItem('nome');
-    const valorComCifrao = `R$ ${localStorage.getItem('valor')}`;
+    roupa.innerHTML = localStorage.getItem('Nome');
+    const valorComCifrao = `R$ ${localStorage.getItem('Preço')}`;
     valor.innerHTML = valorComCifrao;
 
-    Array.from(imagem).forEach((element, i) => {
+    for (let i = 0; i < 3; i++) {
         let imag = document.getElementById('img' + (i + 1));
         let url = window.localStorage.getItem('img' + (i + 1));
-        imag.src = url;
-    })
-}
-);
+        if (imag && url) {
+            imag.src = url;
+        }
+    }
+});
 
 let grandona = window.localStorage.getItem('img4');
 for (let i = 0; i < 3; i++) {
-
     let imagem = document.getElementById('img' + (i + 1));
-
-    imagem.addEventListener('click', e => {
-        imgGrande.src = imagem.src;
-        imagem.src = grandona;
-        grandona = imgGrande.src;
-    });
+    if (imagem) {
+        imagem.addEventListener('click', e => {
+            imgGrande.src = imagem.src;
+            imagem.src = grandona;
+            grandona = imgGrande.src;
+        });
+    }
 }
 
-let botaoCarrinho = getElementById('btnCarrinho');
+let botaoCarrinho = document.getElementById('btnCarrinho');
+if (botaoCarrinho) {
+    botaoCarrinho.addEventListener('click', e => {
+        if (JSON.parse(window.localStorage.getItem('array')) == undefined) {
+            window.localStorage.setItem('array', JSON.stringify([]));
+        }
 
-btnCarrinho.addEventListener('click', e => {
-    if (JSON.parse(window.localStorage.getItem('array')) == undefined){
-        window.localStorage.setItem('array' , JSON.stringify([]));
-    }
+        let arrayGet = JSON.parse(window.localStorage.getItem('array'));
+        let quantidade = Number(document.getElementById('iquantidade').value);
 
-    let arrayGet = JSON.parse(window.localStorage.getItem('array'));
-     let quantidade = Number(document.getElementById('iquantidade').value);
+        let nomeProduto = window.localStorage.getItem('Nome');
+        let valorProduto = Number(window.localStorage.getItem('Preço'));
 
-     let nomeProduto = window.localStorage.getItem('nome')
-     let valorProduto = window.localStorage.getItem('valor')
+        let produtoExistente = arrayGet.find(item => item.nome == nomeProduto);
 
-     let produtoExistente = arrayGet.find(item => item.nome == nomeProduto);
-
-     if (produtoExistente) {
-        produtoExistente.quantidade += quantidade
-        console.log(produtoExistente)
-
-    let valorTotalProduto = valorProduto * produtoExistente.quantidade;
-    produtoExistente.valor = valorTotalProduto
-        }else{
+        if (produtoExistente) {
+            produtoExistente.quantidade += quantidade;
+            produtoExistente.valor = valorProduto * produtoExistente.quantidade;
+        } else {
             let objeto = {
                 nome: nomeProduto,
-                valor: valorTotalProduto,
+                valor: valorProduto * quantidade,
                 quantidade: quantidade,
-            }
-        arrayGet.push(objeto);
+            };
+            arrayGet.push(objeto);
         }
-    window.localStorage.setItem('array', JSON.stringify(arrayGet));
-    console.log(arrayGet)
 
-    alert('Produto adicionado ao carrinho.')
-});
+        window.localStorage.setItem('array', JSON.stringify(arrayGet));
+        alert('Produto adicionado ao carrinho.');
+    });
+}
